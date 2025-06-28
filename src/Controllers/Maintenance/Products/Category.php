a9efe9c36222647eb2287edcbceb470dcdccada9
Vanessa Valladares <100142668+vivq22@users.noreply.github.com>
2025-06-28 10:22:12 -0600

    feat: Enhance Category management with improved data handling and UI updates

--- src/Controllers/Maintenance/Products/Category.php
+++ src/Controllers/Maintenance/Products/Category.php
@@ -6,30 +6,187 @@ use Controllers\PublicController;
 use Dao\Products\Categories as CategoriesDAO;
 use Views\Renderer;
 
+use Utilities\Site;
+use Utilities\Validators;
+
+const LIST_URL = "index.php?page=Maintenance-Products-Categories";
+
 class Category extends PublicController
 {
     private array $viewData;
+    private array $modes;
+    private array $status;
     public function __construct()
     {
-        $this->viewData = [];
+        $this->viewData = [
+            "mode" => "",
+            "id" => 0,
+            "categoria" => "",
+            "estado" => "ACT",
+            "modeDsc" => "",
+            "selectedACT" => "",
+            "selectedINA" => "",
+            "selectedRTR" => "",
+            "errors" => []
+        ];
+        $this->modes = [
+            "INS" => "New Category",
+            "UPD" => "Updating %s",
+            "DEL" => "Deleting %s",
+            "DSP" => "Details of %s"
+        ];
+
+        $this->status = ["ACT", "INA", "RTR"];
     }
     public function run(): void
     {
-        if (isset($_GET["mode"])) {
-            $this->viewData["mode"] = $_GET["mode"];
-            if ($this->viewData["mode"] !== "INS") {
-                $this->viewData["id"] = intval($_GET["id"]);
-            }
-            if ($this->viewData["id"] > 0) {
-                $tmpCategoria = CategoriesDAO::getCategoriesById(
-                    $this->viewData["id"]
-                );
-                if (count($tmpCategoria) > 0) {
-                    $this->viewData["categoria"] = $tmpCategoria["categoria"];
-                    $this->viewData["estado"] = $tmpCategoria["estado"];
-                }
+
+        /*
+        1 - Cargar la data de query params
+        2 - Determinamos el MODO de ejecución del Formulario
+        3 - Si el MODO no es INS
+            3.1 - Cargar los datos de la DB
+        4 - Si la solicitud Postback  (POST | PUT | DELETE)
+            4.1 - Cargar los datos del Body ( POST )
+            4.2 - Validar los datos
+            4.3 - Ejecutar el métodos según el MODO
+            4.4 - Mostrar mensaje y enviar a Listados
+        5 - Prepara la data para la vista
+        6 - Renderizar la Vista
+        */
+        $this->getQueryParamsData();
+        if ($this->viewData["mode"] !== "INS") {
+            $this->getDataFromDB();
+        }
+        if ($this->isPostBack()) {
+            $this->getBodyData();
+            if ($this->validateData()) {
+                //Procesamos
             }
         }
+
+        $this->prepareViewData();
+
+
         Renderer::render("maintenance/products/category", $this->viewData);
+    }
+
+    private function throwError(string $message, string $logMessage = "")
+    {
+        if (!empty($logMessage)) {
+            error_log(sprintf("%s - %s", $this->name, $this->$logMessage));
+        }
+        Site::redirectToWithMsg(LIST_URL, $message);
+    }
+    private function innerError(string $scope, string $message)
+    {
+        if (!isset($this->viewData["errors"][$scope])) {
+            $this->viewData["errors"][$scope] = [$message];
+        } else {
+            $this->viewData["errors"][$scope][] = $message;
+        }
+    }
+
+    private function getQueryParamsData()
+    {
+        if (!isset($_GET["mode"])) {
+            $this->throwError(
+                "Something went wrong, try again.",
+                "Attempt to load controler without the required query parameters MODE"
+            );
+        }
+        $this->viewData["mode"] = $_GET["mode"];
+        if (!isset($this->modes[$this->viewData["mode"]])) {
+            $this->throwError(
+                "Something went wrong, try again.",
+                "Attempt to load controler with  wrong value on query parameter MODE - " . $this->viewData["mode"]
+            );
+        }
+        if ($this->viewData["mode"] !== "INS") {
+            if (!isset($_GET["id"])) {
+                $this->throwError(
+                    "Something went wrong, try again.",
+                    "Attempt to load controler without the required query parameters ID"
+                );
+            }
+            if (!is_numeric($_GET["id"])) {
+                $this->throwError(
+                    "Something went wrong, try again.",
+                    "Attempt to load controler with  wrong value on query parameter ID - " . $_GET["id"]
+                );
+            }
+            $this->viewData["id"] = intval($_GET["id"]);
+        }
+    }
+
+    private function getDataFromDB()
+    {
+        $tmpCategoria = CategoriesDAO::getCategoriesById(
+            $this->viewData["id"]
+        );
+        if ($tmpCategoria && count($tmpCategoria) > 0) {
+            $this->viewData["categoria"] = $tmpCategoria["categoria"];
+            $this->viewData["estado"] = $tmpCategoria["estado"];
+        } else {
+            $this->throwError(
+                "Something went wrong, try again.",
+                "Record for id " . $this->viewData["id"] . " not found."
+            );
+        }
+    }
+
+    private function getBodyData()
+    {
+        if (!isset($_POST["id"])) {
+            $this->throwError(
+                "Something went wrong, try again.",
+                "Trying to post without parameter ID on body"
+            );
+        }
+        if (!isset($_POST["categoria"])) {
+            $this->throwError(
+                "Something went wrong, try again.",
+                "Trying to post without parameter CATEGORY on body"
+            );
+        }
+        if (!isset($_POST["estado"])) {
+            $this->throwError(
+                "Something went wrong, try again.",
+                "Trying to post without parameter ESTADO on body"
+            );
+        }
+        if (intval($_POST["id"]) !== $this->viewData["id"]) {
+            $this->throwError(
+                "Something went wrong, try again.",
+                "Trying to post with inconsistent parameter ID value has: " . $this->viewData["id"] . " recieved: " . $_POST["id"]
+            );
+        }
+
+        $this->viewData["categoria"] = $_POST["categoria"];
+        $this->viewData["estado"] = $_POST["estado"];
+    }
+
+    private function validateData(): bool
+    {
+        if (Validators::IsEmpty($this->viewData["categoria"])) {
+            $this->innerError("categoria", "This field is required.");
+        }
+        if (count($this->viewData["categoria"]) > 255) {
+            $this->innerError("categoria", "Value is too long. Maximun allowed 255 character.");
+        }
+        if (!in_array($this->viewData["estado"], $this->status)) {
+            $this->innerError("estado", "This field is required.");
+        }
+
+        return !(count($this->viewData["errors"]) > 0);
+    }
+    private function prepareViewData()
+    {
+        $this->viewData["modeDsc"] = sprintf(
+            $this->modes[$this->viewData["mode"]],
+            $this->viewData["categoria"]
+        );
+
+        $this->viewData['selected' . $this->viewData["estado"]] = "selected";
     }
 }
--- src/Views/templates/maintenance/products/category.view.tpl
+++ src/Views/templates/maintenance/products/category.view.tpl
@@ -1,4 +1,59 @@
-<h1>Category {{mode}}</h1>
-<div><strong>Id</strong>: {{id}} </div>
-<div><strong>Category</strong>: {{categoria}} </div>
-<div><strong>Status</strong>: {{estado}} </div>
+<section class="depth-2 px-4 py-5">
+    <h2>{{modeDsc}}</h2>
+</section>
+<section class="depth-2 px-4 py-4 my-4 grid row">
+   <form 
+        method="POST"
+        action="index.php?page=Maintenance-Products-Category&mode={{mode}}&id={{id}}"
+        class="grid col-12 col-m-8 offset-m-2 col-l-6 offset-l-3"
+    >
+        <div class="row my-2">
+            <label for="id" class="col-12 col-m-4 col-l-3">Id:</label>
+            <input 
+                type="text"
+                name="id"
+                id="id"
+                value="{{id}}"
+                placeholder="Catégory Id"
+                class="col-12 col-m-8 col-l-9"
+                readonly
+             />
+        </div>
+        <div class="row my-2">
+            <label for="category" class="col-12 col-m-4 col-l-3">Category:</label>
+            <input 
+                type="text"
+                name="categoria"
+                id="category"
+                value="{{categoria}}"
+                placeholder="Name of Category"
+                class="col-12 col-m-8 col-l-9"
+             />
+        </div>
+        <div class="row my-2">
+            <label for="status" class="col-12 col-m-4 col-l-3">Status:</label>
+            <select id="status" name="estado">
+                <option value="ACT" {{selectedACT}}>Active</option>
+                <option value="INA" {{selectedINA}}>Disabled</option>
+                <option value="RTR" {{selectedRTR}}>Retired</option>
+            </select>
+        </div>
+        <div class="row">
+            <div class="col-12 right">
+                <button class="" id="btnCancel" type="button">Cancel</button>
+                &nbsp;
+                <button class="primary" type="submit">Confirm</button>
+           </div>
+        </div>
+    </form>
+</section>
+<script>
+    document.addEventListener("DOMContentLoaded", ()=>{
+        document.getElementById("btnCancel")
+            .addEventListener("click", (e)=>{
+                e.preventDefault();
+                e.stopPropagation();
+                window.location.assign("index.php?page=Maintenance-Products-Categories");
+            });
+    });
+</script>
